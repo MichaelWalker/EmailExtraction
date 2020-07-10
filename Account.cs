11317cb4cefe299e44aa500b08ca36940fcf9b50
Mike Walker <michael.walker@softwire.com>
2020-07-10 23:19:22 +0100

    Add account class.

---
+++ Account.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace EmailExtraction
+{
+    public class Account
+    {
+        public string name;
+        public List<Transaction> incomingTransactions;
+        public List<Transaction> outgoingTransactions;
+
+        public decimal GetTotalIncoming()
+        {
+            var total = new decimal(0);
+
+            foreach (var transaction in incomingTransactions)
+            {
+                total = total + transaction.amount;
+            }
+
+            return total;
+        }
+
+        public decimal GetTotalOutgoing()
+        {
+            var total = new decimal(0);
+
+            foreach (var transaction in outgoingTransactions)
+            {
+                total = total + transaction.amount;
+            }
+
+            return total;
+        }
+
+        public decimal GetTotalToPay()
+        {
+            return GetTotalOutgoing() - GetTotalIncoming();
+        }
+    }
+}
