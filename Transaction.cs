5bf1a1a3796ab197e8cdb6fde01b9ea983f20e63
Mike Walker <michael.walker@softwire.com>
2020-07-10 23:01:49 +0100

    Create transaction class.

---
+++ Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace EmailExtraction
+{
+    public class Transaction
+    {
+        public string date;
+        public string from;
+        public string to;
+        public string narrative;
+        public decimal amount;
+    }
+}
